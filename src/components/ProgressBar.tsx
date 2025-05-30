939e26405bb4fb95626661cc158a4117da3ec7cc
João Victor Fanchini de Azevedo <joaofanchini@gmail.com>
2025-05-30 19:32:29 -0300

    fix: adjusting initial appereance for Progress bar

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -1,18 +1,42 @@
 "use client";
-import { motion, useScroll, useSpring } from "motion/react";
+import { motion, useScroll } from "motion/react";
+import { RefObject, useEffect, useRef, useState } from "react";
+
+const functionToObserver = (
+  h1Ref: RefObject<HTMLHeadingElement | null>,
+  observer: IntersectionObserver
+) => {
+  if (h1Ref.current) {
+    observer.observe(h1Ref.current);
+  }
+};
 
 export default function ProgressBar() {
   const { scrollYProgress } = useScroll();
-  const scaleX = useSpring(scrollYProgress, {
-    stiffness: 100,
-    damping: 30,
-    restDelta: 0.001,
-  });
+  const [isVisible, setIsVisible] = useState(false);
+  const h1Ref = useRef<HTMLHeadingElement>(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(([entry]) => {
+      setIsVisible(!entry.isIntersecting);
+    });
+
+    functionToObserver(h1Ref, observer);
+
+    return () => observer.disconnect();
+  }, [h1Ref]);
 
   return (
-    <motion.div
-      className="fixed top-0 left-0 right-0 bg-teal-600 h-1.5 origin-left z-50"
-      style={{ scaleX: scaleX }}
-    ></motion.div>
+    <>
+      <motion.div
+        className={`fixed rounded top-0 left-0 right-0 bg-teal-600 h-1.5 origin-left z-50 opacity-0 transition-all duration-3 ease-in-out`}
+        style={{ scaleX: scrollYProgress, opacity: isVisible ? 1 : 0 }}
+      >
+        {}
+      </motion.div>
+      <div ref={h1Ref} className="sr-only">
+        Mark
+      </div>
+    </>
   );
 }
